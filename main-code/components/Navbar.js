59db07729e54bef4a4d5acd6730b036b5e153ab8
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-25 18:38:19 +0530

    Update Navbar.js

--- main-code/components/Navbar.js
+++ main-code/components/Navbar.js
@@ -3,21 +3,21 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='w-[80%] h-[10vh] flex flex-row justify-center items-center rounded-2xl bg-white text-black shadow-lg z-10 top-3.5 fixed'>
-        <div className='w-[30%] h-[99%] flex justify-center items-start'>
-            <img src="/logo.svg" alt=""  className='w-full h-[80%]'/>
+        <div className='w-[30%] h-[99%] flex justify-start items-center ml-4'>
+            <img src="/logo.svg" alt=""  className='w-[130px] h-[80%]'/>
         </div>
-        <div className='w-[40%] h-[99%] flex justify-center items-center'>
-            <ul className='w-[60%] h-[99%] flex flex-row justify-start items-center gap-2'>
-                <li className="text-sm">Products</li>
-                <li className="text-sm">Templates</li>
-                <li className="text-sm">Marketplace</li>
-                <li className="text-sm">Learn</li>
-                <li className="text-sm">Pricing</li>
+        <div className='w-[40%] h-[99%] flex justify-start items-center'>
+            <ul className='w-[65%] h-[99%] flex flex-row justify-start items-center gap-2'>
+                <li className="text-[18px]">Products</li>
+                <li className="text-[18px]">Templates</li>
+                <li className="text-[18px]">Marketplace</li>
+                <li className="text-[18px]">Learn</li>
+                <li className="text-[18px]">Pricing</li>
             </ul>
         </div>
         <div className='w-[30%] h-[99%] flex flex-row justify-center items-center gap-1.5'>
-            <button className='w-[49%] h-[99%] flex justify-center items-center bg-gray-600 text-white rounded-full text-sm'>Sign In</button>
-            <button className='w-[49%] h-[99%] flex justify-center items-center bg-blue-600 text-white rounded-full text-sm'>login</button>
+            <button className='w-[25%] h-[80%] flex justify-center items-center bg-gray-600 text-white rounded-full text-sm'>Sign In</button>
+            <button className='w-[25%] h-[80%] flex justify-center items-center bg-blue-600 text-white rounded-full text-sm'>login</button>
         </div>
     </nav>
   )
