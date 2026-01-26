30c2ee8157d7c57f0e5e3779281f11fe767d1622
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-26 12:51:18 +0530

    update

--- main-code/app/page.js
+++ main-code/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <main className="w-full h-auto flex flex-col justify-start items-center gap-10 bg-white text-black">
       <section className="w-[99%] h-[105vh] flex flex-row justify-start items-center bg-[#d2e823] text-[#254f1a]">
         <div className="w-[50%] h-[80%] flex flex-col justify-center items-end pl-20 gap-2.5">
-          <h1 className="w-full h-[75%] flex justify-center items-end text-[73px] font-bold">A link in bio built for you.</h1>
+          <h1 className="w-full h-[50%] flex justify-center items-end text-[73px] h1-font">A link in bio built for you.</h1>
           <p className="w-full h-[15%] flex justify-center items-center font-[17px]">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
-          <div className="w-full h-[10%] flex flex-row justify-start items-center gap-4">
+          <div className="w-full h-[35%] flex flex-row justify-start items-start gap-4">
             <input type="email" name="email" id="" placeholder="linktr.ee/" className="w-[60%] h-[99%] rounded-[10px] bg-white border border-gray-300 px-4 py-2 " />
             <button className="w-[40%] h-[99%] rounded-[10px] bg-[#254f1a] text-white border border-gray-300 px-4 py-2 ">Get Started</button>
           </div>
