f495a0b4e2c0d7689dd0db310b379be0ed70d47d
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-29 21:03:52 +0530

    Update page.js

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const generate = () => {
     return (
-        <section className='w-full h-screen grid grid-cols-2 justify-center items-center py-[10vh] gap-2.5 bg-[#254f1a] text-[#d2e823]'>
+        <section className='w-full h-screen grid grid-cols-2 justify-center items-center py-[5vh] gap-2.5 bg-[#e9c0e9] text-[#d2e823]'>
             <div className="w-[99%] h-[99%] flex justify-center items-center">
 
             </div>
             <div className="w-[99%] h-[80%] flex justify-center items-center overflow-y-hidden">
-                <img src="/generate.png" alt="generate-img" className="w-[80%] h-[460px]" />
+                <img src="/generate.png" alt="generate-img" className="w-[99%] h-[99%]" />
             </div>
         </section>
     )
