6f9c0eebda4ce539fbb5825b338be44287d9d467
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-29 18:41:05 +0530

    Update page.js

--- main-code/app/page.js
+++ main-code/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full h-auto flex flex-col justify-start items-center gap-10 bg-white text-black">
-      <section className="w-full h-[105vh] flex flex-row justify-start items-center bg-[#d2e823] text-[#254f1a]">
+      <section className="w-full h-[105vh] flex flex-row justify-start items-center bg-[#254f1a] text-[#254f1a]">
         <div className="w-[50%] h-[80%] flex flex-col justify-center items-end pl-20 gap-2.5">
           <h1 className="w-full h-[50%] flex justify-center items-end text-[73px] h1-font">A link in bio built for you.</h1>
           <p className="w-full h-[15%] flex justify-center items-center font-[17px] section-1-p">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
@@ -12,8 +12,8 @@ export default function Home() {
             <button className="w-[40%] h-12.5 rounded-[10px] bg-[#254f1a] text-white border border-gray-300 px-4 py-2 ">Get Started</button>
           </div>
         </div>
-        <div className="w-[50%] h-[80%] flex justify-center items-center overflow-y-hidden ">
-          <Image src="/section-1-img.png" alt="section-1-img" className="w-[99%] h-[99%] mix-blend-darken" fill="true"/>
+        <div className="w-[50%] h-[80%] flex justify-center items-center overflow-y-hidden">
+          <Image src="/section-1-img.png" alt="section-1-img" className="w-[80%] h-[460px] mix-blend-darken"/>
         </div>
       </section>
       <section></section>
