858cd0ed0abb4225053441f4be1a21a7b14e3ea3
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-30 21:39:18 +0530

    Update page.js

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -10,20 +10,20 @@ const generate = () => {
                 <div className='w-[99%] h-30 flex flex-col justify-start items-center'>
                     <h2 className='text-xl font-semibold'>Step 1: Claim Your Handle</h2>
                     <div className="my-4 w-[45%] flex flex-col gap-2.5">
-                        <input type="text" placeholder='choose your handle' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
+                        <input type="text" placeholder='choose your handle' className='w-[99%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-while focus:ring-pink-400'/>
                     </div>
                 </div>
                 <div className='w-[99%] h-62.5 flex flex-col justify-start items-center'>
                     <h2 className='text-xl font-semibold'>Step 2: Add Your Links</h2>
                     <div className="my-4 w-[45%] flex flex-col gap-2.5">
-                        <input type="text" placeholder='Enter link text' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
-                        <input type="text" placeholder='Enter link' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
-                        <button className='w-[80%] h-15 rounded-3xl bg-pink-400 text-white font-bold hover:bg-pink-500'>+ Add Link</button>
+                        <input type="text" placeholder='Enter link text' className='w-[99%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-while focus:ring-pink-400'/>
+                        <input type="text" placeholder='Enter link' className='w-[99%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-while focus:ring-pink-400'/>
+                        <button className='w-[99%] h-15 rounded-3xl bg-pink-400 text-white font-bold hover:bg-pink-500'>+ Add Link</button>
                     </div>
                 </div>
                 <div className='w-[99%] h-42.5 flex flex-col justify-start items-center'>
                     <h2 className='text-xl font-semibold'>Step 3: Add Picture and Description</h2>
-                    <input type="text" placeholder='Enter picture URL' className='w-[45%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
+                    <input type="text" placeholder='Enter picture URL' className='w-[45%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-while focus:ring-pink-400'/>
                 </div>
             </div>
             <div className="w-[99%] h-[105vh] flex justify-center items-center overflow-y-hidden">
