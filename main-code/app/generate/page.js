acad52042101251fb9d5fd2a82353d1fdeaec634
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-30 19:55:05 +0530

    Update page.js

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -3,11 +3,11 @@ import React from 'react'
 const generate = () => {
     return (
         <section className='w-full h-auto grid grid-cols-2 justify-center items-center gap-2.5 bg-[#e9c0e9] text-[#261d39] overflow-y-scroll scrollbar-hide'>
-            <div className="w-[99%] h-[105vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl py-[10vh]">
+            <div className="w-[99%] h-[105vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl py-[10vh] gap-2">
                 <div className='w-[99%] h-10 flex justify-center items-center'>
                     <h1 className='text-2xl font-bold'>Generate Your LinkTree</h1>
                 </div>
-                <div className='w-[99%] h-42.5 flex flex-col justify-start items-center'>
+                <div className='w-[99%] h-30 flex flex-col justify-start items-center'>
                     <h2 className='text-xl font-semibold'>Step 1: Claim Your Handle</h2>
                     <div className="my-4 w-[45%] flex flex-col gap-2.5">
                         <input type="text" placeholder='choose your handle' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
