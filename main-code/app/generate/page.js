f1762c3a63e4199fdebf29474463ad7ca7e3fd9a
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-01-30 21:35:46 +0530

    Update page.js

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const generate = () => {
     return (
-        <section className='w-full h-auto grid grid-cols-2 items-center gap-2.5 bg-[#e9c0e9] text-[#261d39] overflow-y-scroll scrollbar-hide justify-center' style={{display: 'grid', justifyContent: 'center', alignItems: 'center'}}>
-            <div className="w-[80%] h-[77vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl gap-2">
+        <section className='w-full h-auto grid grid-cols-2 items-center gap-2.5 bg-[#e9c0e9] text-[#261d39] overflow-y-scroll scrollbar-hide justify-center' style={{display: 'grid', justifyItems: 'center', alignItems: 'center'}}>
+            <div className="w-[80%] h-[77vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl p-1 gap-2">
                 <div className='w-[99%] h-10 flex justify-center items-center'>
                     <h1 className='text-2xl font-bold'>Generate Your LinkTree</h1>
                 </div>
@@ -13,7 +13,7 @@ const generate = () => {
                         <input type="text" placeholder='choose your handle' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
                     </div>
                 </div>
-                <div className='w-[99%] h-42.5 flex flex-col justify-start items-center'>
+                <div className='w-[99%] h-62.5 flex flex-col justify-start items-center'>
                     <h2 className='text-xl font-semibold'>Step 2: Add Your Links</h2>
                     <div className="my-4 w-[45%] flex flex-col gap-2.5">
                         <input type="text" placeholder='Enter link text' className='w-[80%] h-15 pl-2.5 rounded-3xl bg-[#00489dd6] text-[#c66df1de] focus:ring-pink-400'/>
