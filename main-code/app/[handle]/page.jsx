926332f3fb7de2b52012ef860bf5c0249f7e5695
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-02-13 20:56:27 +0530

    update

--- main-code/app/[handle]/page.jsx
+++ main-code/app/[handle]/page.jsx
@@ -1,10 +1,23 @@
 import Link from "next/link"
-// import clientPromise from "@/lib/mongodb"
+import clientPromise from "@/lib/mongodb"
+import { notFound } from "next/navigation";
 
 
 export default async function Page({ params }) {
     const handle = params.handle
-    const item = {
+    const client = await clientPromise
+    if (!client) {
+        return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
+    }
+    const db = client.db("linktree")
+    const collection = db.collection("links")
+    // If the handle is already claimed, you cannot create the bittree
+    const item = await collection.findOne({handle: handle})
+    if(!item){
+        return notFound()
+    }
+
+    const i = {
         "_id": {
             "$oid": "698ebed4d3e4f56704c7a1c3"
         },
@@ -16,18 +29,11 @@ export default async function Page({ params }) {
             },
             {
                 "text": "vercel",
-                "url": "https://github.com/Shashikantgiri-web/LinkTree-Clone"
+                "url": "https://vercel.com/shashikantgiri-webs-projects/linktree/deployments"
             }
         ],
         "linkImages": "https://avatars.githubusercontent.com/u/188772850?v=4&size=64"
     }
-
-    // const client = await clientPromise
-    // if (!client) {
-    //     return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
-    // }
-    // const db = client.db("linktree")
-    // const collection = db.collection("links")
 
     return <div className="w-full h-[105vh] flex flex-col justify-center items-center bg-[#4e8341cb] text-[#d6ed2af1]">
         <div className="w-[80%] h-[77vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl p-2 px-1.5 gap-2">
@@ -36,7 +42,7 @@ export default async function Page({ params }) {
                 <h1 className='text-2xl font-bold'>{item.handle}</h1>
             </div>
             <div className="w-[99%] h-[90%] flex flex-row justify-center items-center">
-                <div className="w-[50%] h-full flex flex-col justify-start items-center gap-2">
+                <div className="w-[50%] h-full flex flex-col justify-start items-center gap-2 overflow-y-scroll scrollbar-hide">
                     {item.links.map((item, index)=>{
                     return <Link  key={index} href= {item.url}><div className="bg-purple-100 text-blue-900 py-4 shadow-lg px-2 min-w-96 flex justify-center rounded-md my-3">
                        {item.text} 
--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react'
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 import 'react-toastify/dist/ReactToastify.css';
 
 const GenerateContent = () => {
@@ -89,7 +90,7 @@ const GenerateContent = () => {
                         <h2 className='text-xl font-semibold'>Step 3: Add Picture and Description</h2>
                         <div className="my-4 w-[99%] flex flex-col gap-2.5">
                             <input type="text" placeholder='Enter picture URL' value={linkImages || ""} onChange={(e) => setlinkImages(e.target.value)} className='w-[65%] h-11.25 pl-2.5 rounded-3xl bg-[#00489dd6] text-white focus:ring-pink-400' />
-                            <button className='disabled:bg-pink-200 w-[40%] h-11.25 rounded-3xl bg-pink-400 text-white font-bold hover:bg-pink-500' onClick={() => submitLink()} disabled={!handle || !links.length || !linkImages}>Submit your linktree</button>
+                            <Link href={`/${handle}`}><button className='disabled:bg-pink-200 w-[40%] h-11.25 rounded-3xl bg-pink-400 text-white font-bold hover:bg-pink-500' onClick={() => submitLink()} disabled={!handle || !links.length || !linkImages}>Submit your linktree</button></Link>
                         </div>
                     </div>
                 </div>
