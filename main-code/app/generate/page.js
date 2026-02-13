0a510be1bad0abc2b91e91bde14fef3dbe6555b2
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-02-13 21:01:25 +0530

    Update page.js

--- main-code/app/generate/page.js
+++ main-code/app/generate/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { Suspense } from 'react'
+import React, { Suspense } from 'react';
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { useSearchParams } from 'next/navigation';
