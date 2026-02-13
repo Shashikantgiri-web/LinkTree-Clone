5641f4452fe586b352f3fced6b121a357ad05855
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-02-13 19:31:01 +0530

    Create page.jsx

---
+++ main-code/app/handle/page.jsx
@@ -0,0 +1,15 @@
+// import { redirect } from "next/navigation"
+import clientPromise from "@/lib/mongodb"
+
+
+export default async function handle({ params }) {
+    const handle = params.handle
+    // const client = await clientPromise
+    // if (!client) {
+    //     return Response.json({ success: false, error: "Database not connected" }, { status: 500 })
+    // }
+    // const db = client.db("linktree")
+    // const collection = db.collection("links")
+
+    return <div>my post : {handle}</div>
+}
