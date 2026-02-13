05d8e6e4de4f73b97270a84c55c43ac3e87b3252
Shashikant Giri <Shashikantshankar707@gmail.com>
2026-02-13 20:16:54 +0530

    Update page.jsx

--- main-code/app/[handle]/page.jsx
+++ main-code/app/[handle]/page.jsx
@@ -29,13 +29,21 @@ export default async function Page({ params }) {
     // const db = client.db("linktree")
     // const collection = db.collection("links")
 
-    return <div className="w-full h-[105vh] flex justify-start items-center bg-[#4e8341cb] text-[#d6ed2af1]">
-        <div className="w-[80%] h-[77vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl p-1 px-1.5 gap-2">
-            <div className='w-[99%] h-[20%] flex flex-col justify-center items-center'>
+    return <div className="w-full h-[105vh] flex flex-col justify-center items-center bg-[#4e8341cb] text-[#d6ed2af1]">
+        <div className="w-[80%] h-[77vh] flex flex-col justify-center items-center bg-transparent border-2 border-pink-300 rounded-3xl p-2 px-1.5 gap-2">
+            <div className='w-[99%] h-[25%] flex flex-col justify-center items-center'>
                 <img src={item.linkImages} alt="img" className="w-[12%] h-[82%] rounded-[40%]" />
                 <h1 className='text-2xl font-bold'>{item.handle}</h1>
             </div>
-            <div className="w-[99%] h-[90%] flex flex-row justify-center items-center"></div>
+            <div className="w-[99%] h-[90%] flex flex-row justify-center items-center">
+                <div className="w-[50%] h-full flex flex-col justify-start items-center gap-2">
+                    {item.links.map((item, index)=>{
+                    return <Link  key={index} href= {item.url}><div className="bg-purple-100 py-4 shadow-lg px-2 min-w-96 flex justify-center rounded-md my-3">
+                       {item.text} 
+                    </div></Link> 
+                })}
+                </div>
+            </div>
         </div>
     </div>
 }
